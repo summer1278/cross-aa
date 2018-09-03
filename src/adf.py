e2a382009f10fb7ab889d884f4ff6df4b68f037c
Xia <cxcui188@gmail.com>
2018-09-03 22:01:31 +0100

    update

---
+++ src/adf.py
@@ -0,0 +1,16 @@
+"""
+Augmented Doppelgänger Finder Implementation
+(Overdorf and Greenstadt, 2016)
+
+ Xia Cui
+ 09/2018
+"""
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+def classify():
+    LogisticRegression(n_jobs=-1,solver='lbfgs')
+    pass
+
+def mixed_training():
+    pass
